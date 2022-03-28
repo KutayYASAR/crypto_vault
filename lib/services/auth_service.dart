af3de5e47a49972b3b6ee48b4b01ad2d2562e3d3
SemihYlmaz <56646018+SemihYlmaz@users.noreply.github.com>
2022-03-28 18:15:29 +0300

    working on firebase connections

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -18,10 +18,20 @@ class AuthService {
     var user = await _auth.createUserWithEmailAndPassword(
         email: email, password: password);
 
-    await _firestore.collection("Vault Data").doc(user.user!.uid).set(
-        {'email': email, 'nameSurname': nameSurname, 'vaultName': vaultName});
-
+    await _firestore
+        .collection("Vaults")
+        .doc(user.user!.uid)
+        .collection(vaultName)
+        .doc(nameSurname)
+        .set({
+      'email': email,
+    });
     return user.user;
+  }
+
+  //Giriş yapılı olan kullanıcıyı alan fonksiyon
+  User? getCurrentUser() {
+    return _auth.currentUser!;
   }
 
   //çıkış yap fonksiyonu
